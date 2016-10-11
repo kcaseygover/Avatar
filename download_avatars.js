d44ec864c60a00d38863c3931f2bbbd3024c697b
Karen Casey-Gover <karen.casey@hotmail.com>
2016-10-11 21:40:56 +0000

    downloaded github avatars to local file

--- download_avatars.js
+++ download_avatars.js
@@ -2,39 +2,39 @@ var request = require('request');
 var reposOwner = process.argv[2];
 var reposName = process.argv[3];
 var endpoint = `/repos/${reposOwner}/${reposName}/contributors`;
-
+var githubRoot = "https://api.github.com";
+var fs = require('fs');
 
 function githubRequest(endpoint, callback) {
-  var githubRoot = "https://api.github.com";
-
   var options = {
     url: `${githubRoot}${endpoint}`,
     headers: {
       'User-Agent': 'request'
     }
   };
-
-
   request.get(options, callback);
 }
 
-function getGithubContributors(reposowner, reposname,  callback) {
+function getRepoContributors(reposowner, reposname,  callback) {
   githubRequest(endpoint, callback);
 }
 
+function downloadImageByUrl (url, filePath) {
+  request.head(url, function(err, res, body){
+    var fileNameExt = res.headers['content-type'].split('/').pop();
 
-getGithubContributors(reposOwner, reposName, function(error, response, body) {
+  request(url).pipe(fs.createWriteStream(`${filePath}.${fileNameExt}`));
+  });
+};
+getRepoContributors(reposOwner, reposName, function(error, response, body) {
   if (error) {
     console.log("Something went wrong:", error);
     return;
   }
   var contributors = JSON.parse(body);
-
-  console.log(contributors);
-
   contributors.forEach(function(contributor){
-    console.log(`- ${contributor.avatar_url}`);
+    console.log(`${contributor.avatar_url}`);
+    downloadImageByUrl(contributor.avatar_url, `avatar/${contributor.login}`);
   });
 });
-console.log(`Getting ${reposOwner}/${reposName}/ contributors...\n`);
 
